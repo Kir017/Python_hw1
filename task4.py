6df205600282fd1b855ce2debfd3d7935ee59e3a
Kirill Kovalev <kir017@mail.ru>
2023-02-21 22:52:54 +0300

    поправил

--- task4.py
+++ task4.py
@@ -5,10 +5,3 @@ if number % 6 == 0:
     katiy = number - number / 3
     print (f'Петя сделал {int (petr)} , Сергей сделал {int (sergey)} , Катя сделала {int(katiy)}')
 else: print ('Не корректное количество журавлей')
-
-
-# a = int(input())
-# k = int((a/3)*2)
-# p = int((k/2)/2)
-# s = int(p)
-# print(p, k, s)
